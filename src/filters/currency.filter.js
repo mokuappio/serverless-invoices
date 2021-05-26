f25f73ca3bec4ae29949391f00bc7f738a9c936d
Marek Fraczyk <mareksmakosz@gmail.com>
2021-05-26 16:26:28 +0300

    fix currency filter rounding

--- src/filters/currency.filter.js
+++ src/filters/currency.filter.js
@@ -1,9 +1,11 @@
 export function formatCurrency(val, digits = 2) {
   if (val !== null) {
-    const x = parseFloat(val);
+    let x = parseFloat(val);
     if (Number.isNaN(x)) {
       return '';
     }
+    const decimalLimiter = 10 ** digits;
+    x = Math.round(x * decimalLimiter) / decimalLimiter;
     const parts = x.toFixed(digits).split('.');
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
     return parts.join('.');
