4aaa06c3e9aafe994e36691fe6188a7fd63049bd
Marek Fraczyk <mareksmakosz@gmail.com>
2021-05-26 16:47:53 +0300

    currency.filter.js - fix edge case rounding

--- src/filters/currency.filter.js
+++ src/filters/currency.filter.js
@@ -5,7 +5,7 @@ export function formatCurrency(val, digits = 2) {
       return '';
     }
     const decimalLimiter = 10 ** digits;
-    x = Math.round(x * decimalLimiter) / decimalLimiter;
+    x = Math.round((x + Number.EPSILON) * decimalLimiter) / decimalLimiter;
     const parts = x.toFixed(digits).split('.');
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
     return parts.join('.');
