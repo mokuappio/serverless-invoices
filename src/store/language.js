6d2be77b1b5a86918a7607016f6347e8b951283c
Jean-francois lefebvre <lefebvre.jf@gmail.com>
2021-06-19 12:12:10 +0200

    -- french translations

--- src/store/language.js
+++ src/store/language.js
@@ -1,30 +1,31 @@
 import app from '../main';
 
 export default {
-  namespaced: true,
-  state: {
-    lang: null,
-    all: [
-      { name: 'English', code: 'en' },
-      { name: 'Estonian', code: 'et' },
-      { name: 'Persian', code: 'fa' },
-      { name: 'Spanish', code: 'es' },
-      { name: 'Bangla', code: 'bn' },
-    ],
-  },
-  mutations: {
-    lang(state, lang) {
-      state.lang = lang;
+    namespaced: true,
+    state: {
+        lang: null,
+        all: [
+            { name: 'English', code: 'en' },
+            { name: 'French', code: 'fr' },
+            { name: 'Estonian', code: 'et' },
+            { name: 'Persian', code: 'fa' },
+            { name: 'Spanish', code: 'es' },
+            { name: 'Bangla', code: 'bn' },
+        ],
     },
-  },
-  actions: {
-    changeLanguage({ commit }, lang) {
-      app.$i18n.i18next.changeLanguage(lang.code);
-      app.$router.push({ query: { ...app.$route.query, lang: lang.code } });
-      commit('lang', lang);
+    mutations: {
+        lang(state, lang) {
+            state.lang = lang;
+        },
     },
-    initLanguage({ commit, state }, code) {
-      commit('lang', state.all.find(lang => lang.code === code));
+    actions: {
+        changeLanguage({ commit }, lang) {
+            app.$i18n.i18next.changeLanguage(lang.code);
+            app.$router.push({ query: {...app.$route.query, lang: lang.code } });
+            commit('lang', lang);
+        },
+        initLanguage({ commit, state }, code) {
+            commit('lang', state.all.find(lang => lang.code === code));
+        },
     },
-  },
 };
